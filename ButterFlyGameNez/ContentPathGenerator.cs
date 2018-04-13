79a5475c20fdfa37fec83e8344380e232f4a8666
urvaius <urvaius@gmail.com>
2018-04-13 10:59:13 -0700

    messing with tiled map decide on this

--- ButterFlyGameNez/ContentPathGenerator.cs
+++ ButterFlyGameNez/ContentPathGenerator.cs
@@ -3,26 +3,27 @@
 
 namespace Nez
 {
-	class Content
-	{
-		public static class Textures
-		{
-			public const string butterfly1 = @"Textures\butterfly1";
-			public const string caveman = @"Textures\caveman";
-			public const string powerup = @"Textures\powerup";
-		}
+    class Content
+    {
+        public static class Textures
+        {
+            public const string butterfly1 = @"Textures\butterfly1";
+            public const string caveman = @"Textures\caveman";
+            public const string powerup = @"Textures\powerup";
+        }
 
-		public static class Tiled
-		{
-			public const string tiledMap = @"Tiled\tiledMap";
-			public const string tiledMap3 = @"Tiled\tiledMap3";
-			public const string tiledMapnew = @"Tiled\tiledMapnew";
-			public const string tileset = @"Tiled\tileset";
-			public const string tilesheed2 = @"Tiled\tilesheed2";
-		}
+        public static class Tiled
+        {
+            public const string medieval_tilesheet = @"Tiled\medieval_tilesheet";
+            public const string rtsmedftesting = @"Tiled\rtsmedftesting";
+            public const string tiledMap = @"Tiled\tiledMap";
+            public const string tiledMapnew = @"Tiled\tiledMapnew";
+            public const string tileset = @"Tiled\tileset";
+            public const string tilesheet_complete = @"Tiled\tilesheet_complete";
+        }
 
 
-	}
+    }
 }
 
 
